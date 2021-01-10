8c1b34820c7846ed5070f7bb5df5abde3667facc
LJ <1789831043@qq.com>
2021-01-10 22:49:46 +0800

    以post形式获取数据，接受长字符串

--- src/main/java/com/lw/maillist/controller/BaseController.java
+++ src/main/java/com/lw/maillist/controller/BaseController.java
@@ -7,10 +7,7 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -18,6 +15,48 @@ public class BaseController {
 
     @GetMapping("/test/{listStr}")
     public Maillist Parse(@PathVariable String listStr){
+        System.out.println("输入的字符串" + listStr);
+        // 对每一个输入的字符串，构造一个 ANTLRStringStream 流 in
+        ANTLRInputStream input = new ANTLRInputStream(listStr);
+        // 用 in 构造词法分析器 lexer，词法分析的作用是将字符聚集成单词或者符号
+        MaillistLexer lexer = new MaillistLexer(input);
+        lexer.removeErrorListeners();
+        MyLexErrorListener myLexErrorListener = new MyLexErrorListener();
+        lexer.addErrorListener(myLexErrorListener);
+        // 用词法分析器 lexer 构造一个记号流 tokens
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // 再使用 tokens 构造语法分析器 至此parser,已经完成词法分析和语法分析的准备工作
+        MaillistParser parser = new MaillistParser(tokens);
+        parser.removeErrorListeners();
+        MyParseErrorListener myParseErrorListener = new MyParseErrorListener();
+        parser.addErrorListener(myParseErrorListener);
+        // 最终调用语法分析器的规则maillist（这个是我们在Maillist.g4里面定义的那个规则），完成对邮箱地址列表的验证
+        ParseTree parserTree = parser.maillist();
+
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        MaillistBaseListener maillistBaseListener = new MaillistBaseListener();
+        walker.walk(maillistBaseListener, parserTree);
+        for (String s: myParseErrorListener.errorList) {
+            System.out.println("sdfd"+s);
+        }
+
+        //构造json数据
+        Maillist maillist = new Maillist();
+
+        maillist.mailBoxes = maillistBaseListener.mailBoxes;
+        maillist.errMsg = myParseErrorListener.errorList;
+        maillist.hasError = false;
+        if (maillistBaseListener.isError.contains("true")){
+            maillist.hasError = true;
+        }
+
+        return maillist;
+    }
+
+    @PostMapping("/test1")
+    public Maillist Parse2(@RequestBody OriginMaillist originMaillist){
+        String listStr = originMaillist.getOriginMaillist();
         System.out.println("输入的字符串" + listStr);
         // 对每一个输入的字符串，构造一个 ANTLRStringStream 流 in
         ANTLRInputStream input = new ANTLRInputStream(listStr);
---
+++ src/main/java/com/lw/maillist/grammar/OriginMaillist.java
@@ -0,0 +1,19 @@
+package com.lw.maillist.grammar;
+
+/**
+ * @program: maillist
+ * @author: LJ
+ * @create: 2021-01-10 22:45
+ **/
+public class OriginMaillist {
+
+    public String originMaillist;
+
+    public String getOriginMaillist() {
+        return originMaillist;
+    }
+
+    public void setOriginMaillist(String originMaillist) {
+        this.originMaillist = originMaillist;
+    }
+}
